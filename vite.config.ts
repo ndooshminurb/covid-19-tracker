5175a385da2c44302edf2ca883a8e8101079150e
Kot <117808752+ndooshminurb@users.noreply.github.com>
2022-12-13 20:55:21 +0700

    First commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/covid-19/',
+  base: '/covid-19-tracker/',
   plugins: [react(), tsconfigPaths()],
 });
